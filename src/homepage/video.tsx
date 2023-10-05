3105331c1843d472ed6413e3139fa91813afc465
CodeSandbox <support@codesandbox.io>
2023-10-05 01:16:16 +0000

    Sandbox snapshot commit

---
+++ src/homepage/video.tsx
@@ -0,0 +1,14 @@
+export default function Video(){
+    return(
+        <div>
+        <video autoPlay loop muted>
+        <source src="src/assets/4c749533161fc77c899a376ec6cd6da38973772f.mp4" type="video/mp4" />
+        {/* Add additional source elements for other video formats if needed */}
+        {/* <source src="/path/to/your-video.webm" type="video/webm" /> */}
+        {/* <source src="/path/to/your-video.ogv" type="video/ogg" /> */}
+        Your browser does not support the video tag.
+      </video> 
+        </div>
+     
+    )
+}
