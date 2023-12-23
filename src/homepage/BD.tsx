12d288cdfbb2e2ae246c997f259cdd0080159a58
CodeSandbox <support@codesandbox.io>
2023-12-23 06:03:02 +0000

    Initial commit

--- src/homepage/BD.tsx
+++ src/homepage/BD.tsx
@@ -1,35 +1,32 @@
-
-    
-    
-    export default function BD(){
-    return(
-        <div className="below1">
-           <div className="below11">
-           
-            <section>
-                <p>BASIC/DEPT® is a global branding and digital design agency building products, services, and eCommerce experiences that turn cultural values into company value.</p>
-            <button>See The Work</button>
-            </section>
-            <h1>B/D</h1> 
-          
+export default function BD() {
+  return (
+    <div className="below1">
+      <div className="below11">
+        <section id="basictext">
+          <p>
+            BASIC/DEPT® is a global branding and digital design agency building
+            products, services, and eCommerce experiences that turn cultural
+            values into company value.
+          </p>
+          <button>SEE THE WORK</button>
+        </section>
+        <h1>B/D®</h1>
+      </div>
+      <div className="pictureline">
+        <div>
+          <img src="src/assets/photo1.png" alt="" />
+          <h1> PATAGONIA</h1>
         </div>
-          <div className="pictureline">
-                <div> 
-                    <img src='src/assets/photo1.png' alt="" />
-                    <h1> PATAGONIA</h1>
-
-                    
-                </div>
-                <div className="photo2">
-                    <img src="src/assets/photo2.png" alt="" />
-                    <h1>WILSON</h1>
-                </div>
-                <div>
-                    <img src="src/assets/photo3.png" alt="" />
-                    <h1>GOOGLE STORE</h1>
-                </div>Q2 
-           </div>
-        
+        <div className="photo2">
+          <img src="src/assets/photo2.png" alt="" />
+          <h1>WILSON</h1>
         </div>
-    )
+        <div>
+          <img src="src/assets/photo3.png" alt="" />
+          <h1>GOOGLE STORE</h1>
+        </div>
+        Q2
+      </div>
+    </div>
+  );
 }
