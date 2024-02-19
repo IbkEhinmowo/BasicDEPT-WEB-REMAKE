6c5a657ed389f04d29f50b2eaf7ee3da5974e62f
Ibukun <142057631+IbkEhinmowo@users.noreply.github.com>
2024-02-19 21:57:48 +0000

    feat: Remove offscreen push, add responsive styling for elements and correct syntax  for video element

--- src/homepage/video.tsx
+++ src/homepage/video.tsx
@@ -1,14 +1,16 @@
-export default function Video(){
-    return(
-        <div>
-        <video autoPlay loop muted>
-        <source src="src/assets/4c749533161fc77c899a376ec6cd6da38973772f.mp4" type="video/mp4" />
+export default function Video() {
+  return (
+    <div>
+      <video autoPlay loop muted playsInline>
+        <source
+          src="src/assets/4c749533161fc77c899a376ec6cd6da38973772f.mp4"
+          type="video/mp4"
+        />
         {/* Add additional source elements for other video formats if needed */}
         {/* <source src="/path/to/your-video.webm" type="video/webm" /> */}
         {/* <source src="/path/to/your-video.ogv" type="video/ogg" /> */}
         Your browser does not support the video tag.
-      </video> 
-        </div>
-     
-    )
+      </video>
+    </div>
+  );
 }
