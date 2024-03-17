be13a5523eb8815bdd76139f0cdd65e015f59617
Ibukun <142057631+IbkEhinmowo@users.noreply.github.com>
2024-03-17 05:10:11 +0000

    update

--- src/homepage/video.tsx
+++ src/homepage/video.tsx
@@ -3,10 +3,8 @@ export default function Video() {
     <div>
       <video autoPlay loop muted playsInline>
         <source src="src/assets/special.mp4" type="video/mp4" />
-        {/* Add additional source elements for other video formats if needed */}
-        {/* <source src="/path/to/your-video.webm" type="video/webm" /> */}
-        {/* <source src="/path/to/your-video.ogv" type="video/ogg" /> */}
       </video>
+      <button id="playbutton">Book now</button>
     </div>
   );
 }
