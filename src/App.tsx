b23c8a8f739a5b0446286e813a066573423c4934
Ibukun <134826455+Stygian102@users.noreply.github.com>
2024-05-24 20:06:51 +0000

    feat: Add a test comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,14 @@ import Taskbar from "./homepage/Taskbar";
 import Video from "./homepage/video";
 import BD from "./homepage/BD";
 
-
-
-
 function App() {
-
-
   return (
     <div className="App">
-  
-      <Taskbar/>
-      <Video/>
-      <BD/>
+      <Taskbar />
+      <Video />
+      <BD />
     </div>
-
+    //test
   );
 }
 
